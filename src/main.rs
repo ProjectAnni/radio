eb271512fa839329a339c7741e6a3f729b27d693
Yesterday17 <mmf@mmf.moe>
2021-11-07 14:06:04 +0800

    ffmpeg transcode to s16le directly instead of wav use reference instead of Arc

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,14 @@ use std::io::Write;
 use tokio::process::{ChildStdout, Command};
 use anni_backend::{Backend, BackendReader, BackendReaderExt};
 use anni_backend::backends::FileBackend;
+use anni_repo::Album;
+use anni_repo::album::Track;
 use rand::Rng;
 use tempfile::{NamedTempFile, TempPath};
 use tokio::fs::File;
-use tokio::io::AsyncWriteExt;
 use crate::repo::RepoManager;
 
-fn random_song(albums: &HashSet<String>, repo: &RepoManager) -> (String, usize) {
+fn random_song<'repo>(albums: &HashSet<String>, repo: &'repo RepoManager) -> (String, usize, &'repo Album, &'repo Track) {
     loop {
         let mut rng = rand::thread_rng();
         let pos = rng.gen_range(0..albums.len());
@@ -29,7 +30,7 @@ fn random_song(albums: &HashSet<String>, repo: &RepoManager) -> (String, usize) 
                 use anni_repo::album::TrackType;
                 match track.track_type() {
                     TrackType::Normal => {
-                        return (catalog.clone(), track_id);
+                        return (catalog.clone(), track_id, album, track);
                     }
                     _ => continue,
                 }
@@ -38,37 +39,34 @@ fn random_song(albums: &HashSet<String>, repo: &RepoManager) -> (String, usize) 
     }
 }
 
-async fn to_wav(mut reader: BackendReader) -> anyhow::Result<ChildStdout> {
+async fn to_s16le(mut reader: BackendReader) -> anyhow::Result<ChildStdout> {
     let mut cmd = Command::new("ffmpeg")
         .args([
             // "-re",
             "-f", "flac",
             "-i", "pipe:0",
-            "-f", "wav",
-            // TODO: pcm_s16le 44100 stereo s16
+            "-f", "s16le",
+            "-ac", "2",
+            "-ar", "44100",
             "pipe:1",
         ])
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .stderr(Stdio::null())
         .spawn()?;
-    let mut stdin = cmd.stdin.take().expect("Failed to take stdin of ffmpeg(to_wav)");
+    let mut stdin = cmd.stdin.take().expect("Failed to take stdin of ffmpeg(to_s16le)");
     tokio::spawn(async move {
         let _ = tokio::io::copy(&mut reader, &mut stdin).await;
         drop(stdin);
     });
-    let mut stdout = cmd.stdout.take().expect("Failed to take stdout of ffmpeg(to_wav)");
-    // TODO: calculate wav header size instead of fixed 196 offset
-    utils::skip(&mut stdout, 196).await?;
+    let stdout = cmd.stdout.take().expect("Failed to take stdout of ffmpeg(to_s16le)");
     Ok(stdout)
 }
 
-async fn generate_cover(catalog: String, track_id: usize, manager: Arc<RepoManager>, backend: Arc<FileBackend>) -> anyhow::Result<(BackendReaderExt, ChildStdout)> {
+async fn generate_cover<'repo>(backend: Arc<FileBackend>, (catalog, track_id, album, track): (String, usize, &'repo Album, &'repo Track)) -> anyhow::Result<(BackendReaderExt, ChildStdout)> {
     let audio = backend.get_audio(&catalog, track_id as u8).await?;
     let mut cover = backend.get_cover(&catalog).await?;
 
-    let album = manager.load_album(&catalog).unwrap();
-    let track = &album.discs()[0].tracks()[track_id - 1];
     // TODO: i18n support for text
     let text_temp_file = tempfile::NamedTempFile::new()?;
     let mut text_file = text_temp_file.as_file();
@@ -132,19 +130,21 @@ async fn save_cover(mut cover: ChildStdout) -> anyhow::Result<TempPath> {
 async fn main() -> anyhow::Result<()> {
     let manager = RepoManager::new(env::var("ANNI_REPO")?);
     let mut backend = FileBackend::new(PathBuf::from(env::var("ANNI_RADIO_ROOT")?), false);
-    let albums = Arc::new(backend.albums().await?);
+    let albums = backend.albums().await?;
 
     let mut process = Command::new("ffmpeg")
         .args([
             "-hide_banner",
             "-re",
             "-async", "1",
-            "-thread_queue_size", "512",
+            // "-thread_queue_size", "512",
             "-f", "image2",
             "-loop", "1",
             "-framerate", "25",
             "-i", "cover.png",
-            "-f", "wav",
+            "-f", "s16le",
+            "-ac", "2",
+            "-ar", "44100",
             "-i", "pipe:0",
             "-c:v", "libx264",
             // "-c:v", "h264_omx",
@@ -167,24 +167,16 @@ async fn main() -> anyhow::Result<()> {
     });
 
     let mut stdin = process.stdin.take().expect("Failed to take stdin of ffmpeg(main)");
-    let manager = Arc::new(manager);
     let backend = Arc::new(backend);
-
-    let wave_header = [
-        0x52, 0x49, 0x46, 0x46, 0xFF, 0xFF, 0xFF, 0xFF, 0x57, 0x41, 0x56, 0x45, 0x66, 0x6D, 0x74, 0x20,
-        0x10, 0x00, 0x00, 0x00, 0x01, 0x00, 0x02, 0x00, 0x44, 0xAC, 0x00, 0x00, 0x10, 0xB1, 0x02, 0x00,
-        0x04, 0x00, 0x10, 0x00, 0x64, 0x61, 0x74, 0x61, 0xFF, 0xFF, 0xFF, 0xFF,
-    ];
-    stdin.write_all(&wave_header).await.expect("Failed to write WAVE header");
 
     const PLAYLIST_SIZE: usize = 2;
     let mut playlist = VecDeque::with_capacity(PLAYLIST_SIZE);
 
     loop {
         if playlist.len() != PLAYLIST_SIZE {
-            let (catalog, track_id) = random_song(&albums.clone(), &manager.clone());
-            eprintln!("catalog = {}, track = {}", catalog, track_id);
-            if let Ok((audio, cover)) = generate_cover(catalog, track_id, manager.clone(), backend.clone()).await {
+            let song = random_song(&albums, &manager);
+            eprintln!("catalog = {}, track = {}", song.0, song.1);
+            if let Ok((audio, cover)) = generate_cover(backend.clone(), song).await {
                 if let Ok(cover) = save_cover(cover).await {
                     playlist.push_back((audio, cover));
                 }
@@ -196,7 +188,7 @@ async fn main() -> anyhow::Result<()> {
             let (audio, cover) = playlist.pop_front().unwrap();
             // TODO: do not ?
             tokio::fs::copy(cover, "cover.png").await?;
-            let mut stdout = to_wav(audio.reader).await?;
+            let mut stdout = to_s16le(audio.reader).await?;
             tokio::io::copy(&mut stdout, &mut stdin).await?;
         }
     }
